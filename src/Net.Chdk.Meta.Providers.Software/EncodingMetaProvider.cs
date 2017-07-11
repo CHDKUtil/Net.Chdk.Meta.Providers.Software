452cbc4ada9708379c71b3c3dbc5e3050c54a1e1
Dmitry Shechtman <damnian@gmail.com>
2017-07-11 03:54:15 +0300

    Replace SoftwareEncodingInfo with SoftwareInfo

--- src/Net.Chdk.Meta.Providers.Software/EncodingMetaProvider.cs
+++ src/Net.Chdk.Meta.Providers.Software/EncodingMetaProvider.cs
@@ -1,8 +1,5 @@
-﻿using Net.Chdk.Meta.Providers;
-using Net.Chdk.Model.Software;
-using System;
+﻿using Net.Chdk.Model.Software;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Net.Chdk.Meta.Providers.Software
 {
@@ -11,6 +8,7 @@ namespace Net.Chdk.Meta.Providers.Software
         #region Fields
 
         private IBootMetaProvider BootProvider { get; }
+        private Dictionary<uint, SoftwareEncodingInfo> Encodings { get; }
 
         #endregion
 
@@ -19,42 +17,33 @@ namespace Net.Chdk.Meta.Providers.Software
         public EncodingMetaProvider(IBootMetaProvider bootProvider)
         {
             BootProvider = bootProvider;
-
-            _encodings = new Lazy<SoftwareEncodingInfo[]>(GetEncodings);
-            _encodingsDictionary = new Lazy<Dictionary<uint, SoftwareEncodingInfo>>(GetEncodingsDictionary);
+            Encodings = new Dictionary<uint, SoftwareEncodingInfo>();
         }
 
         #endregion
 
         #region IEncodingMetaProvider Members
 
-        public SoftwareEncodingInfo GetEncoding(SoftwareEncodingInfo encoding)
+        public SoftwareEncodingInfo GetEncoding(SoftwareInfo software)
         {
-            return EncodingsDictionary[encoding.Data ?? 0];
+            var key = software.Encoding.Data ?? 0;
+            SoftwareEncodingInfo encoding;
+            if (!Encodings.TryGetValue(key, out encoding))
+            {
+                encoding = GetEncoding(software.Encoding.Data);
+                Encodings.Add(key, encoding);
+            }
+            return encoding;
         }
 
         #endregion
 
         #region Encodings
 
-        private readonly Lazy<SoftwareEncodingInfo[]> _encodings;
-
-        private SoftwareEncodingInfo[] Encodings => _encodings.Value;
-
-        private SoftwareEncodingInfo[] GetEncodings()
+        private static SoftwareEncodingInfo GetEncoding(uint? data)
         {
-            var length = BootProvider.Offsets != null
-                ? BootProvider.Offsets.Length
-                : 1;
-            return Enumerable.Range(0, length)
-                .Select(GetEncoding)
-                .ToArray();
-        }
-
-        private SoftwareEncodingInfo GetEncoding(int i)
-        {
-            return i > 0
-                ? GetEncoding(BootProvider.Offsets[i - 1])
+            return data.HasValue
+                ? GetEncoding(data.Value)
                 : GetEmptyEncoding();
         }
 
@@ -66,35 +55,13 @@ namespace Net.Chdk.Meta.Providers.Software
             };
         }
 
-        private static SoftwareEncodingInfo GetEncoding(int[] offsets)
+        private static SoftwareEncodingInfo GetEncoding(uint offsets)
         {
             return new SoftwareEncodingInfo
             {
                 Name = "dancingbits",
-                Data = GetOffsets(offsets)
+                Data = offsets,
             };
-        }
-
-        private static uint? GetOffsets(int[] offsets)
-        {
-            var uOffsets = 0u;
-            for (int index = 0; index < offsets.Length; index++)
-                uOffsets += (uint)offsets[index] << (index << 2);
-            return uOffsets;
-        }
-
-        #endregion
-
-        #region EncodingsDictionary
-
-        private readonly Lazy<Dictionary<uint, SoftwareEncodingInfo>> _encodingsDictionary;
-
-        private Dictionary<uint, SoftwareEncodingInfo> EncodingsDictionary => _encodingsDictionary.Value;
-
-        private Dictionary<uint, SoftwareEncodingInfo> GetEncodingsDictionary()
-        {
-            return Encodings
-                .ToDictionary(e => e.Data ?? 0, e => e);
         }
 
         #endregion
